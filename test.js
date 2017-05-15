1350366eda7d4c34a9fc77fd2e1d2d5271a58c01
chouchou723 <527835300@qq.com>
2017-05-15 09:22:56 +0800

    Update test.js

--- test.js
+++ test.js
@@ -13,21 +13,15 @@ rem 解决方案。
 
 
 
- body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, th, td { margin:0; padding:0; }
- 2 body, button, input, select, textarea { font:12px/1.5tahoma, arial, \5b8b\4f53; }
- 3 h1, h2, h3, h4, h5, h6{ font-size:100%; }
- 4 address, cite, dfn, em, var { font-style:normal; }
- 5 code, kbd, pre, samp { font-family:couriernew, courier, monospace; }
- 6 small{ font-size:12px; }
- 7 ul, ol { list-style:none; }
- 8 a { text-decoration:none; }
- 9 a:hover { text-decoration:underline; }
-10 sup { vertical-align:text-top; }
-11 sub{ vertical-align:text-bottom; }
-12 legend { color:#000; }
-13 fieldset, img { border:0; }
-14 button, input, select, textarea { font-size:100%; }
-15 table { border-collapse:collapse; border-spacing:0; } 
+body,ol,ul,h1,h2,h3,h4,h5,h6,p,th,td,dl,dd,form,fieldset,legend,input,textarea,select{margin:0;padding:0}
+body{font:12px"宋体","Arial Narrow",HELVETICA;background:#fff;-webkit-text-size-adjust:100%;}
+a{color:#2d374b;text-decoration:none}
+a:hover{color:#cd0200;text-decoration:underline}
+em{font-style:normal}
+li{list-style:none}
+img{border:0;vertical-align:middle}
+table{border-collapse:collapse;border-spacing:0}
+p{word-wrap:break-word}
 	  
 css web初始。
 
